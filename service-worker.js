fad47509fd7d86fd1b8eb973d00185ccf1a75602
musa sutisna <mus4.sutisn4@gmail.com>
2019-11-03 01:31:25 +0700

    Add to home screen

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,14 @@ const CACHE_NAME = 'pocketnote-v0.0.1'
 let urlsToCache = [
   '/',
   '/css/materialize.min.css',
+  '/images/icon-72x72.png',
+  '/images/icon-96x96.png',
+  '/images/icon-128x128.png',
+  '/images/icon-144x144.png',
+  '/images/icon-192x192.png',
+  '/images/icon-256x256.png',
+  '/images/icon-384x384.png',
+  '/images/icon-512x512.png',
   '/images/sick.png',
   '/images/business.jpg',
   '/images/note.jpg',
@@ -37,6 +45,23 @@ self.addEventListener('fetch', function (event) {
         console.log(`Service worker: load asset from server (${event.request.url})`)
 
         return fetch(event.request)
+      })
+  )
+})
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function (cacheNames) {
+        return Promise.all(
+          cacheNames.map(function (cacheName) {
+            if (cacheName !== CACHE_NAME) {
+              console.log(`Service worker: cache ${cacheName} deleted`)
+
+              return caches.delete(cacheName);
+            }
+          })
+        )
       })
   )
 })
